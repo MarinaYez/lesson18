89213192e2cb4ba2f9e535fbd389fc9f9fd4885f
Marina Yezerska <118081961+MarinaYez@users.noreply.github.com>
2022-12-30 02:19:55 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// let firsMonht  = prompt("Enter first month salary"); //first month salary
+// let secondtMonth = prompt("Enter second month salary");
+// let result =(+firsMonht + + secondtMonth);
+// alert(`Your salary for 2 months is: ${result}`);
+// result += 1;
+// let = confirm(`You have extra bonus! Your total salary was: 1300$ it's increase for 1$ and now it is: ${result} $`)
+// const selIf = result >= 2000;
+// const selIfT = result !== 2000;
+// console.log(selIf);
+// if (selIf) {
+// let = confirm(`Will you work next? ${selIf}`)
+// } else {
+//     let = confirm(`Will you work next? ${selIfT}`)
+// }
+
+let firsMonht  = prompt("Enter first month salary"); //first month salary
+let secondtMonth = prompt("Enter second month salary");
+let result =(+firsMonht + + secondtMonth);
+alert(`Your salary for 2 months is: ${result}`);
+result += 1;
+let = confirm(`You have extra bonus! Your total salary was: 1300$ it's increase for 1$ and now it is: ${result} $`)
+const selIf = result < 2000;
+const selIfT = result > 2000;
+console.log(selIf);
+console.log(selIfT);
+if (selIf) {
+let = confirm(`I'm out `)
+} else  {
+let = confirm(`I'm ready to work `)
+}
+    
+
+
+
